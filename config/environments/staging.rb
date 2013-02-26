1ff74dec0e1a5bf9169d25931efd33f90f776a24
Christian Blavier <cblavier@gmail.com>
2013-02-26 11:44:26 +0100

    fixed staging config file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -72,7 +72,6 @@ Folyo::Application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV['SENDGRID_DOMAIN']
     :domain         => 'heroku.com'
   }
 
