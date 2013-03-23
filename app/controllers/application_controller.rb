523bcd10da64e415af234fa2239e80f4d6a6fcb2
Christian Blavier <cblavier@gmail.com>
2013-03-23 18:27:13 +0100

    fixed after_sign_in_path for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if session[:previous_url]
       session[:previous_url]
+    elsif current_user.is_a?(Admin)
+      admin_offers_path
     else
       offers_path
     end
