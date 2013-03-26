753508cdfdc4f8e1fd93e40bb1e07f265897ee48
Christian Blavier <cblavier@gmail.com>
2013-03-26 18:11:30 +0100

    changed vero async setup

--- config/initializers/vero.rb
+++ config/initializers/vero.rb
@@ -3,10 +3,12 @@
     config.api_key = "f35fcb3f3333cc79c22684becb2e3b8c83b56ff7"
     config.secret = "4208b8c3c3e0daf865f8287f22efab2403148c44"
 
-    config.async = :delayed_job
-
     if Rails.env.test?
       config.async = :none
+    elsif Rails.env.staging?
+      config.async = :thread
+    else
+      config.async = :delayed_job
     end
 
     if Rails.env.development? || Rails.env.test? || Rails.env.staging?
