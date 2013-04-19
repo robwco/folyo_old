1a43e71355bfa6a0e4bc8595a5913f34314179c3
Christian Blavier <cblavier@gmail.com>
2013-04-19 10:44:36 +0200

    fix mixpanel people set

--- lib/event_tracker.rb
+++ lib/event_tracker.rb
@@ -12,7 +12,7 @@ class EventTracker < Struct.new(:name, :properties, :request_env, :user_data)
   def self.track_user_action(user, name, properties, request_env = nil)
     unless Rails.env.test?
       user.track(name, properties)
-      user_data = user.to_json(only: %i{email full_name created_at}, methods: %i{role profile_url})
+      user_data = {email: user.email, full_name: user.full_name, created_at: user.created_at, role: user.role, profile_url: user.profile_url}
       Delayed::Job.enqueue MixpanelWorker.new(name, properties, request_env, user_data), queue: 'mixpanel'
     end
   end
--- lib/mixpanel_worker.rb
+++ lib/mixpanel_worker.rb
@@ -5,16 +5,9 @@ class MixpanelWorker < Struct.new(:name, :properties, :request_env, :user_data)
     if defined?(MIXPANEL_TOKEN)
       mixpanel = Mixpanel::Tracker.new(MIXPANEL_TOKEN, env: request_env)
 
+      properties[:distinct_id] = user_data[:email] if user_data.has_key?(:email)
       mixpanel.track(name, properties)
-
-      unless user_data.empty?
-        mixpanel.set(user_data['email'], {
-          email:       user_data['email'],
-          created_at:  user_data['created_at'],
-          role:        user_data['role'],
-          profile_url: user_data['profile_url']
-        })
-      end
+      mixpanel.set(user_data[:email], user_data) if user_data.has_key?(:email)
     end
 
   end
