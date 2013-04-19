e7028348dfe2436162a44ace7ed80e8ec2b1da2d
Christian Blavier <cblavier@gmail.com>
2013-04-19 15:45:10 +0200

    change default value for mixpanel env

--- lib/event_tracker.rb
+++ lib/event_tracker.rb
@@ -3,13 +3,13 @@ require 'mixpanel_worker'
 # Common event tracking system for Vero and MixPanel services
 class EventTracker < Struct.new(:name, :properties, :request_env, :user_data)
 
-  def self.track_action(name, properties, request_env = nil)
+  def self.track_action(name, properties, request_env = {})
     unless Rails.env.test?
       Delayed::Job.enqueue MixpanelWorker.new(name, properties, request_env, {}), queue: 'mixpanel'
     end
   end
 
-  def self.track_user_action(user, name, properties, request_env = nil)
+  def self.track_user_action(user, name, properties, request_env = {})
     unless Rails.env.test?
       user.track(name, properties)
       user_data = {email: user.email, full_name: user.full_name, created_at: user.created_at, role: user.role, profile_url: user.profile_url}
