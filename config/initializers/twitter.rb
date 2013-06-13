cbc718878a40279528a02a567e90722ec4bc3f03
Christian Blavier <cblavier@gmail.com>
2013-06-13 09:35:54 +0200

    added twitter credentials

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "zrf3o2rhs6n413b8uF9lw"
+  config.consumer_secret = "H38TKkRnhJvBsdlVXFbo0Og3sCL8WRU0SBEOXaSq7r4"
+  config.oauth_token = "372180766-5hTA5bfrWThfDwILskDgRG11xpBI60IxqGXvB1Gi"
+  config.oauth_token_secret = "2nqN51FTCCd9Cnzrz2z2mUfakg2Za0QwfsjmnD9Q"
+end
