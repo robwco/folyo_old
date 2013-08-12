11fe47579b477a5152337305747b0b76bc0be632
Sacha Greif <sacha357@gmail.com>
2013-08-12 11:00:55 +0900

    refactoring CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@
 
 module ApplicationHelper
 
-  def current_link_to(title, target)
+  def current_link_to(title, target, link_class='')
     # see http://www.liquidfoot.com/2010/09/28/add-highlight-link-to-current-page-in-rails/
     # and http://api.rubyonrails.org/classes/ActionView/Helpers/UrlHelper.html#method-i-link_to_unless_current
     link_to_unless_current title, target do
-      link_to title, target, :class => "current"
+      link_to title, target, :class => "current "+link_class
     end
   end
 
