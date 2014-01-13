a82b3f1b142e96bd0f0acdab1183982d310d02ed
Christian Blavier <cblavier@gmail.com>
2014-01-13 15:00:50 +0100

    fixed vero event tracking

--- lib/event_tracker.rb
+++ lib/event_tracker.rb
@@ -11,7 +11,7 @@ class EventTracker < Struct.new(:name, :properties, :request_env, :user_data)
 
   def self.track_user_action(user, name, properties, request_env = {})
     unless Rails.env.test?
-      user.track(name, properties)
+      user.track_user_event(name, properties)
       #user_data = {email: user.email, full_name: user.full_name, created_at: user.created_at, role: user.role, profile_url: user.profile_url}
       #Delayed::Job.enqueue MixpanelWorker.new(name, properties, request_env, user_data), queue: 'mixpanel'
     end
