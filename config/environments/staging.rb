d982eedbf0b13c7ac2540b4e8ba6f40f674a1586
Christian Blavier <cblavier@gmail.com>
2014-02-08 09:16:45 +0100

    cloudfront cdn on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -47,7 +47,7 @@ Folyo::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "//folyo-staging.s3.amazonaws.com"
+  config.action_controller.asset_host = "//d13llhq3exiomo.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
