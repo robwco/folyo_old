084408ff65623ca2fd277228cb3feb54c1097ba3
Christian Blavier <cblavier@gmail.com>
2014-02-10 17:51:07 +0100

    updated asset host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,8 @@ Folyo::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "//folyo-production.s3.amazonaws.com"
+  #config.action_controller.asset_host = "//folyo-production.s3.amazonaws.com"
+  config.action_controller.asset_host = "//assets%d.folyo.me"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
