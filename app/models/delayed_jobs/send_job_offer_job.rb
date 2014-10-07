01ed4759f6315e90910cf7bd30ed9ebaa0043ef7
Christian Blavier <cblavier@gmail.com>
2014-10-07 16:39:29 +0200

    Fixed logging for delayed job

--- app/models/delayed_jobs/send_job_offer_job.rb
+++ app/models/delayed_jobs/send_job_offer_job.rb
@@ -9,11 +9,11 @@ module DelayedJobs
       if job_offer = JobOffer.find(params[:job_offer_id])
 
         designers = Designer.accepted.subscribed_for(job_offer.skills)
-        Rails.logger.debug "Sending #{job_offer.title} to #{designers.count} designers"
+        run.logger.debug "Sending #{job_offer.title} to #{designers.count} designers"
 
         designers.each_slice(200) do |designers|
           designer_emails = designers.map(&:email)
-          Rails.logger.debug "Sending to #{designer_emails}"
+          run.logger.debug "Sending to #{designer_emails}"
           JobOfferMailer.new_job_offer(job_offer.id, designer_emails).deliver
         end
       end
