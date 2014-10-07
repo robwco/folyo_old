7e10bc6b9b79f1051c6964c6e99660ae0f83aba2
Christian Blavier <cblavier@gmail.com>
2014-10-07 16:25:43 +0200

    fix intercom

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,7 +59,7 @@ class User
   def async_track_user_event(event, properties = {})
     vero.events.track!(identity: {id: self.id.to_s}, event_name: event, data: properties) unless Rails.env.test?
     if Rails.env.production?
-      Intercom::Event.create(event_name: event, email: self.email, metadata: properties )
+      Intercom::Event.create(event_name: event, email: self.email, created_at: DateTime.now, metadata: properties )
     end
   end
 
